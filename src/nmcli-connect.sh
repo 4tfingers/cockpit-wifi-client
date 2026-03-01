93842d7491604aaa7587273ce262775ba1e42ba8
4tfingers <4tfingers@gmail.com>
2026-03-02 10:19:09 +1100

    Update nmcli-connect.sh
    added ifname wlan1 to commands

--- src/nmcli-connect.sh
+++ src/nmcli-connect.sh
@@ -9,7 +9,7 @@ if [[ $3 == 'true' ]]; then
   AUTOCONNECT=yes
   # Disable autoconnect for other connections
   for con_id in `nmcli c | grep -o -- "[0-9a-fA-F]\{8\}-[0-9a-fA-F]\{4\}-[0-9a-fA-F]\{4\}-[0-9a-fA-F]\{4\}-[0-9a-fA-F]\{12\}"`;
-    do nmcli connection mod uuid $con_id connection.autoconnect no ; done
+    do nmcli connection mod uuid $con_id connection.autoconnect no ifname wlan1 ; done
 else
   AUTOCONNECT=no
 fi
@@ -19,10 +19,10 @@ MASK=$6
 GATEWAY=$7
 if [[ $DHCP == 'true' ]]; then
   nmcli con del "$SSID" 1&>/dev/null
-  echo "nmcli con add type wifi ssid "$SSID" con-name "$SSID" autoconnect $AUTOCONNECT "$PASSWORD""
-  nmcli con add type wifi ssid "$SSID" con-name "$SSID" autoconnect $AUTOCONNECT $PASSWORD
+  echo "nmcli con add type wifi ssid "$SSID" con-name "$SSID" autoconnect $AUTOCONNECT "$PASSWORD" ifname wlan1"
+  nmcli con add type wifi ssid "$SSID" con-name "$SSID" autoconnect $AUTOCONNECT $PASSWORD ifname wlan1
 else
-  nmcli con del "$SSID"
-  nmcli con add type wifi ssid "$SSID" con-name "$SSID" autoconnect $AUTOCONNECT ip4 "$IP"/"$MASK" gw4 "$GATEWAY" $PASSWORD
+  nmcli con del "$SSID" ifname wlan1
+  nmcli con add type wifi ssid "$SSID" con-name "$SSID" autoconnect $AUTOCONNECT ip4 "$IP"/"$MASK" gw4 "$GATEWAY" $PASSWORD ifname wlan1
 fi
-nmcli con up "$SSID"
+nmcli con up "$SSID" ifname wlan1
